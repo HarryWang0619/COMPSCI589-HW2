34748440b56f71055ceb1a15e635823f86a45cc4
Harry <harrywang0619@gmail.com>
2022-03-09 03:21:27 -0500

    cleaning up terminal output template

--- run.py
+++ run.py
@@ -12,23 +12,23 @@ def naive_bayes(ptrain:float=0.1,ntrain:float=0.1,ptest:float=0.1,ntest:test=0.1
 	percentage_positive_instances_test  = ptest
 	percentage_negative_instances_test  = ntest
 
-	print("Loading Data, at time ", (0))
+	print("Loading Data, at time 0.00 sec")
 
 	(pos_train, neg_train, vocab) = load_training_set(percentage_positive_instances_train, percentage_negative_instances_train)
 	(pos_test,  neg_test)         = load_test_set(percentage_positive_instances_test, percentage_negative_instances_test)
 
-	print("Number of positive training instances:", len(pos_train))
-	print("Number of negative training instances:", len(neg_train))
-	print("Number of positive test instances:", len(pos_test))
-	print("Number of negative test instances:", len(neg_test))
+	# print("Number of positive training instances:", len(pos_train))
+	# print("Number of negative training instances:", len(neg_train))
+	# print("Number of positive test instances:", len(pos_test))
+	# print("Number of negative test instances:", len(neg_test))
 
 	with open('vocab.txt','w') as f: # modified so it don't print a empty line in the last.
 		f.write("%s" % vocab[0])
 		for word in vocab[1:]:
 			f.write("\n%s" % word)
-	print("Vocabulary (training set):", len(vocab))
+	# print("Vocabulary (training set):", len(vocab))
 	
-	print("Learning Training Data, at time ", (time.time()-t0))
+	print("Learning Training Data, at time ", format((time.time()-t0),".2f"), "sec")
 
 	posidict, negadict = train(pos_train, neg_train)
 	
@@ -41,8 +41,7 @@ def naive_bayes(ptrain:float=0.1,ntrain:float=0.1,ptest:float=0.1,ntest:test=0.1
 	falseNegative = 0
 
 	# i = 0
-	print("Now testing... at time ", (time.time()-t0))
-	print("Testing Positive")
+	print("Testing Positive, at time ", format((time.time()-t0),".2f"), "sec")
 	for posiinstance in positest:
 		ppositive = probilityof(pos_train,neg_train,posidict,negadict,'positive',posiinstance,logbool,laplacesmooth,smoothconst)
 		pnegative = probilityof(pos_train,neg_train,posidict,negadict,'negative',posiinstance,logbool,laplacesmooth,smoothconst)
@@ -51,7 +50,7 @@ def naive_bayes(ptrain:float=0.1,ntrain:float=0.1,ptest:float=0.1,ntest:test=0.1
 		else:
 			falseNegative += 1
 
-	print("Testing Negative, at time ", (time.time()-t0))
+	print("Testing Negative, at time ", format((time.time()-t0),".2f"), "sec")
 	for negainstance in negatest:
 		ppositive = probilityof(pos_train,neg_train,posidict,negadict,'positive',negainstance,logbool,laplacesmooth,smoothconst)
 		pnegative = probilityof(pos_train,neg_train,posidict,negadict,'negative',negainstance,logbool,laplacesmooth,smoothconst)
@@ -65,13 +64,14 @@ def naive_bayes(ptrain:float=0.1,ntrain:float=0.1,ptest:float=0.1,ntest:test=0.1
 	rec = recall(truePositive,trueNegative,falsePositive,falseNegative)
 	f = fscore(truePositive,trueNegative,falsePositive,falseNegative,1)
 
-	print("accuarcy is ",acc)
-	print("precision is ",pre)
-	print("recall is ",rec)
-	print("fscore is ", f)
-	print("total time cose is ", (time.time()-t0), "sec")
+	print("Total Time Cost is ", format((time.time()-t0),".2f"), "sec")
+	print("Accuarcy  is: ", format(acc,".6f"))
+	print("Precision is: ",format(pre,".6f"))
+	print("Recall    is: ",format(rec,".6f"))
+	print("F-Score   is: ", format(f,".6f"))
+
 	return acc
 
 if __name__=="__main__":
-	naive_bayes(0.3,0.3,0.5,0.5)
+	naive_bayes(0.3,0.3,0.2,0.2)
 
